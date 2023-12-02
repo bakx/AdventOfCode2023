7e36ee103198fdc1468f37edcb6ecec8309423bc
Gideon Bakx <gideon@bakx.ca>
2023-12-02 13:10:01 -0700

    Day 1, part 2

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -7,44 +7,198 @@ string[] items = reader
 
 reader.Close();
 
+Func<int, int> Inc = (a) => { a++; return a; };
+Func<int, int> Dec = (a) => { a--; return a; };
 
-int sum = 0;
+Dictionary<string, List<string>> digitText = new()
+{
+    { "1", new() { "1", "one" }},
+    { "2", new() { "2", "two" }},
+    { "3", new() { "3", "three" }},
+    { "4", new() { "4", "four" }},
+    { "5", new() { "5", "five" }},
+    { "6", new() { "6", "six" }},
+    { "7", new() { "7", "seven" }},
+    { "8", new() { "8", "eight" }},
+    { "9", new() { "9", "nine" }},
+};
+
+
+int sumPart1 = 0;
+int sumPart2 = 0;
 
 for (int i = 0; i < items.Length; i++)
 {
-    sum += GetSomething(items[i]);
+    //sumPart1 += GetPart(items[i]);
+    sumPart2 += GetPart(items[i], false);
 }
 
-Console.WriteLine($"Part 1: sum {sum}");
+Console.WriteLine($"Part 1: sum {sumPart1}");
+Console.WriteLine($"Part 2: sum {sumPart2}");
 
 
-int GetSomething(string str)
+int GetPart(string str, bool digitOnly = true)
 {
-    string first = GetDigit(str);
-    string last = GetDigit(str, true);
+    string first = GetDigitOrText(str, digitOnly);
+    string last = GetDigitOrText(str, digitOnly, true);
+
+    if (first.Length == 0 || last.Length == 0)
+    {
+        Console.WriteLine($"---------");
+        Console.WriteLine($"Did not retrieve result, str={str} first={first} last={last}");
+        Environment.Exit(-1);
+    }
+
+    Console.WriteLine($"{str} = {first} + {last}");
 
     return int.Parse(first + last);
 }
 
-string GetDigit(string str, bool reverse = false)
+string GetDigitOrText(string str, bool digitOnly = true, bool reverse = false)
 {
-    if (reverse)
+    int startPosition = reverse ? str.Length : -1;
+    int endPosition = reverse ? 0 : str.Length;
+    Func<int, int> loopDirection = reverse ? Dec : Inc;
+
+    int i = startPosition;
+
+    while (true)
     {
-        for (int i = str.Length; i > 0; i--)
+        i = loopDirection(i);
+
+        if (reverse && i < 0)
+            break;
+
+        if (i == endPosition)
+            break;
+        
+        if (!reverse && i > endPosition)
+            break;
+
+        if (char.IsNumber(str[i]))
+            return str[i].ToString();
+
+        if (digitOnly)
+            continue;
+
+        string hasWord = "";
+        int lengthLeft = 0;
+
+        if (reverse)
         {
-            if (Char.IsNumber(str[i - 1]))
-                return str[i - 1].ToString();
+            lengthLeft = i - endPosition + 1;
+
+            if (lengthLeft < 1)
+            {
+                continue; // No words fit
+            }
+
+            int maxGet = Math.Min(lengthLeft, 5);
+            int minGet = Math.Max(i - maxGet + 1, 0);
+            hasWord = str.Substring(minGet, maxGet);
+        }
+        else
+        {
+            lengthLeft = endPosition - i;
+
+            if (lengthLeft < 1)
+            {
+                continue; // No words fit
+            }
+
+            int maxGet = Math.Min(lengthLeft, 5);
+            hasWord = str.Substring(i, maxGet);
         }
 
-        return "";
+        string hasword = isTextDigit(hasWord, reverse);
+        
+        if (hasword.Length > 0)
+            return hasword;
     }
 
-    for (int i = 0; i < str.Length; i++)
+    return "";
+}
+
+string isTextDigit(string str, bool reverse)
+{
+    foreach(List<string> s in digitText.Values)
     {
-        if (Char.IsNumber(str[i]))
-            return str[i].ToString();
+        for (int i = 1; i < s.Count; i++)
+        {
+            string word = s[i];
+
+            if (str.Contains(word))
+            {
+                // Check what index this is at
+                int wordIndex = str.IndexOf(word);
+                int digitIndex = -1;
+
+                // Are there any digits in the substring?
+                if (reverse)
+                {
+                    for (int j = str.Length; j < 0; j++)
+                    {
+                        if (char.IsNumber(str[j]))
+                        {
+                            digitIndex = j;
+                            break;
+                        }
+                    }
+
+                    if (digitIndex < 0)
+                        return s[0];
+
+                    return wordIndex <= digitIndex ? s[0] : str[digitIndex].ToString();
+                }
+                else
+                {
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (char.IsNumber(str[j]))
+                        {
+                            digitIndex = j;
+                            break;
+                        }
+                    }
+
+                    if (digitIndex < 0)
+                        return s[0];
+
+                    return digitIndex <= wordIndex ? str[digitIndex].ToString() : s[0];
+                }
+            }
+        }
     }
+
+    // No matches for any words, how about just digits
+
+    int firstDigitIndex = -1;
+    int lastDigitIndex = -1;
     
+    for (int j = 0; j < str.Length; j++)
+    {
+        if (char.IsNumber(str[j]))
+        {
+            if (firstDigitIndex < 0)
+            {
+                firstDigitIndex = j;
+                continue;
+            }
+            
+            lastDigitIndex = j;
+        }
+    }
+
+    if (firstDigitIndex >= 0)
+    {
+        if (firstDigitIndex >= 0 && lastDigitIndex == -1)
+        {
+            return str[firstDigitIndex].ToString();
+        }
+
+        return reverse ? str[lastDigitIndex].ToString() : str[firstDigitIndex].ToString();
+    }
+
     return "";
 }
 
